2cd2907291f9557e72da0cc597560f1a8f6318ce
vvilip <phillipsf@tutanota.com>
2025-09-09 11:40:30 +0200

    🚧 Try adding Recorder

--- runtime/src/main/java/io/quarkiverse/rage4j/runtime/wrapper/RageAssert.java
+++ runtime/src/main/java/io/quarkiverse/rage4j/runtime/wrapper/RageAssert.java
@@ -1,9 +1,17 @@
 package io.quarkiverse.rage4j.runtime.wrapper;
 
+import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
+
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+import io.quarkiverse.rage4j.runtime.TestClassNameRecorder;
 
 @ApplicationScoped
 public class RageAssert {
+
+    @Inject
+    TestClassNameRecorder testClassNameRecorder;
 
     private String question;
     private String groundTruth;
@@ -19,6 +27,15 @@ public class RageAssert {
     }
 
     public RageAssertionCaller threshold(double threshold) {
+        saveTestClassName();
         return new RageAssertionCaller(question, groundTruth, threshold);
+    }
+
+    private void saveTestClassName() {
+        String testClassName = StackWalker
+                .getInstance(RETAIN_CLASS_REFERENCE)
+                .getCallerClass()
+                .getName();
+        testClassNameRecorder.setClassName(testClassName);
     }
 }
